7886790a2708d0fb812ff41d0f52c3a54e71a807
Robert Lima <61129273+digitalrobertlima@users.noreply.github.com>
2021-06-03 18:43:38 -0300

    up var CryptoJS

--- programa_gerador/1_PrivateKey_Generator_bytes_and_hex.js
+++ programa_gerador/1_PrivateKey_Generator_bytes_and_hex.js
@@ -1,8 +1,7 @@
+var CryptoJS = require('cryptojs').Crypto
 var nomePrograma = '\n Seja muito bem-vindo usuário. Este programa te ajudará na geração de chaves privadas de 32 bytes.\n\n >>> PROGRAMA - DK <<<\n\n >>> GERADOR DE CHAVES PRIVADAS <<<\n >>> EM BYTES E HEXADECIMAL <<<'
 
 console.log(nomePrograma)
-
-var CryptoJS = require('cryptojs').Crypto
 
 var chavePrivada = CryptoJS.util.randomBytes(32)
 
